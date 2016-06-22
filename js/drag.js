82cbd8ae83fd0b53fddc2157172a5ef6419f9ef3
songbingyi <43463006@qq.com>
2016-06-22 23:48:07 +0800

    Drag
    T BUG

---
+++ js/drag.js
@@ -0,0 +1,41 @@
+function getByClass(clasName,parent){
+ var oParent = parent?document.getElementById(parent):document,
+     eles = [],
+	 elements = oParent.getElementsByTagName('*');
+	 
+ for(var i=0,l=elements.length;i<l;i++){
+	 if(elements[i].className == clasName){
+		 eles.push(elements[i]);}
+	 };
+ return eles;
+}
+window.onload = drag;
+function drag(){
+	var oTitle = getByClass('login_logo_webqq','loginPanel')[0];
+	oTitle.onmousedown = fnDown;
+	};
+
+function fnDown(event){
+	var oDrag = document.getElementById("loginPanel");
+	event = event || window.event;
+		var disX = event.clientX - oDrag.offsetLeft;
+	var disY =  event.clientY - oDrag.offsetTop;
+	document.onmousemove = function(event){
+		event = event || window.event;
+		var winW = document.body.clientWidth || document.documentElement.clientWidth;
+		var winH = document.body.clientHeight || document.documentElement.clientHeight
+		var t = event.clientY - disY;
+		var l = event.clientX - disX;
+		if(l<1){
+			l=0;
+			}else if(l > winW - oDrag.offsetWidth){
+				l = winW - oDrag.offsetWidth};
+		if(t<1){
+			t=0;
+			}else if(t > winH - oDrag.offsetHeight){
+				t = winW - oDrag.offsetHeight;}
+		oDrag.style.top = t + "px";
+		oDrag.style.left = l + "px";
+		}
+	}
+	
