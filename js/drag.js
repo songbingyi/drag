1c88638554a26fea14c54a0cbbce8a98115b52ea
songbingyi <43463006@qq.com>
2016-07-16 23:49:37 +0800

    asda
    sd

--- js/drag.js
+++ js/drag.js
@@ -9,16 +9,40 @@ function getByClass(clasName,parent){
 	 };
  return eles;
 }
+
 window.onload = drag;
 function drag(){
 	var oTitle = getByClass('login_logo_webqq','loginPanel')[0];
 	var closeBtn = document.getElementById("ui_boxyClose");
-	var loginState = document.getElementById("loginState");
+	var loginState = document.getElementById("loginState"),
+	loginTxt = document.getElementById("login2qq_state_txt"),
+	statelist =document.getElementById("loginStatePanel"),
+	lis = statelist.getElementsByTagName("li"),
+	stateshow = document.getElementById("loginStateShow");
+	
+	for(var i=0;i<lis.length;i++){
+		lis[i].onmouseover = function(){
+			this.style.backgroundColor = "#FF0000";
+			}
+		lis[i].onmouseout = function(){
+			this.style.backgroundColor = "";
+			}
+		lis[i].onclick = function(){
+			loginTxt.innerHTML = this.childNodes[3].innerHTML;
+			stateshow.className = "login-state-show"+" "+this.id;
+			statelist.style.display = "none";
+			}
+		}	
+	
+		
 	oTitle.onmousedown = fnDown;
 	oTitle.onmouseup = fnUp;	
 	closeBtn.onclick = fClose;
-	loginState.onclick = changeState; //µã»÷ÇÐ»»×´Ì¬
-	}
+	loginState.onclick = changeState;
+}
+
+	
+	
 function fnUp(){
 	document.onmousemove = null;
 	document.onmouseup = null;
@@ -52,7 +76,5 @@ function fClose(){
 
 function changeState(){
 	document.getElementById("loginStatePanel").style.display = "block";
-	var stateP = document.getElementById("loginStatePanel");
-	var states = stateP.childNodes;
-	alert(states[0].nodeName);
+
 }
